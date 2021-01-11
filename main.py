999d75e75a3d47618de53ff790dc260d0e0dead2
陈浩 <kukuikan@qq.com>
2021-01-11 14:22:41 +0800

    MyFirstPython

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*-coding=utf-8-*-
+# @Time:2021/1/913:12
+# @Author:Hchan
+# @File:MyFirstPython.py
+# @software:PyCharm
+
+print("我的年龄是%d岁" % 30)
+print("我的名字是%s，我的国籍是%s" % ("陈浩", "中国"))
+print("aaa", "bbb", 'ccc')
+print("www", "baidu", 'com', sep='.')  # 空格替换成"."
+
+print("Hello ", end='')  # end=''代表不换行
+print('end...')
+
+a = int(input("请输入一组数字："))
+a = a + 100
+print("输入数字加上100 = %d" % a)
